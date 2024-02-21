181f27e2c322bc5b15b8140e6d4c93bca73a9308
AndreyKhamid <poldmitriy999@gmail.com>
2024-02-21 21:38:34 +0300

    Added game_v2.py and game.py from tuskdata

--- project_0/game.py
+++ project_0/game.py
@@ -1,22 +1,22 @@
-""""Игра угадай число"""
+"""Игра угадай число"""
 
 import numpy as np
 
-number = np.random.randint(1, 101) # Загадываем число
+number = np.random.randint(1, 101) # загадываем число
 
-# Количество попыток
+# количество попыток
 count = 0
 
 while True:
-    count += 1
-    predict_number = int(input('Угадай число от 1 до 100: '))
+    count+=1
+    predict_number = int(input("Угадай число от 1 до 100: "))
     
     if predict_number > number:
-        print('Число должно быть меньше!')
-        
+        print("Число должно быть меньше!")
+
     elif predict_number < number:
-        print('Число должно быть больше!')
-        
+        print("Число должно быть больше!")
+    
     else:
-        print(f'Вы угадали число! Это число {number}! Вы справились за {count} попытку(-ки, -ок).')
-        break # Конец игры, выход из цикла
+        print(f"Вы угадали число! Это число = {number}, за {count} попыток")
+        break #конец игры выход из цикла
