e31cfa6d5a4e45cba9b57e9025dc4eafad870604
Khamidulin Andrey <160411802+AndreyKhamid@users.noreply.github.com>
2024-05-07 22:01:02 +0300

    rename PROJECT-0/

---
+++ PROJECT-0. Угадай число/game.py
@@ -0,0 +1,24 @@
+"""Игра угадай число"""
+
+# Импорт библиотеки numpy.
+import numpy as np
+
+# Загадываем рандомное число.
+number = np.random.randint(1, 101)
+
+# Количество попыток.
+count = 0
+
+while True:
+    count+=1
+    predict_number = int(input("Угадай число от 1 до 100: "))
+    
+    if predict_number > number:
+        print("Число должно быть меньше!")
+
+    elif predict_number < number:
+        print("Число должно быть больше!")
+    
+    else:
+        print(f"Вы угадали число! Это число = {number}, за {count} попыток")
+        break #конец игры выход из цикла
